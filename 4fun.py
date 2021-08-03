f03368714d1e70dcfffdecfe64a1b1a1e0b38840
Maor Kadosh <git@avocadosh.xyz>
2021-08-03 15:09:36 +0300

    add changes

--- 4fun.py
+++ 4fun.py
@@ -1,9 +1,61 @@
-#!/usr/bin/env python3
+"""
+Example post representation in the database:
 
-from flask import Flask, render_template
+{
+  "title": "Based Post",
+  "content": "allo",
+  "tags": [
+    "based",
+    "entrepreneur"
+  ],
+  "upvotes": 0,
+  "downvotes": 0,
+  "reports": 0,
+  "id": 123245123124,
+  "parent": 85718975819651, // null if the post is a thread
+  "image": null
+}
+"""
+
+from flask import Flask, render_template, redirect
+from flask import request as req
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+mongoclient = MongoClient()
+db = mongoclient["4fun"]
+posts = db["posts"]
+counters = db["counters"]
+
+if counters.estimated_document_count() == 0:
+    counters.insert_one({"coll": "posts", "last_index": 0})
+
+
+def id_for_new_post():
+    """
+    Get a new unused ID for a post. IDs are auto incremented
+    """
+    last_indx_doc = counters.find_one_and_update({"coll": "posts"},
+                                                 {"$inc": {"last_index": 1}},
+                                                 new=True)
+    return last_indx_doc["last_index"]
+
+
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def index():
+    return render_template("index.html", posts=posts.find({}))
+
+
+@app.route("/new-post", methods=["POST"])
+def new_post():
+    posts.insert_one({
+        "title": req.form["title"],
+        "content": req.form["content"],
+        "tags": [],
+        "upvotes": 0,
+        "downvotes": 0,
+        "reports": 0,
+        "id": id_for_new_post()
+    })
+    return redirect("/")
